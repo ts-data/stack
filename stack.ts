dd89b956e4998cce36d1def47713e3ba9fe191c3
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-12 17:40:52 +0100

    Initial tests

---
+++ stack.ts
@@ -0,0 +1,15 @@
+export class Stack {
+
+    public isEmpty(): boolean {
+        return true;
+    }
+
+    public push(value: any): void {
+
+    }
+
+    public pop(): any {
+
+    }
+
+}
---
+++ test.ts
@@ -0,0 +1,45 @@
+import { Expect, Test } from "alsatian";
+import { Stack } from "./stack";
+
+export class IsEmptyTestFixture {
+
+    @Test()
+    public newStackShouldBeEmpty(): void {
+        let stack = new Stack();
+
+        Expect(stack.isEmpty()).toBe(true);
+    }
+
+    @Test()
+    public emptyStackShouldBeEmpty(): void {
+        let stack = new Stack();
+
+        // add a single element, then remove it again
+        stack.push(5);
+        stack.pop();
+
+        Expect(stack.isEmpty()).toBe(true);
+    }
+
+    @Test()
+    public stackWithOneItemShouldNotBeEmpty(): void {
+        let stack = new Stack();
+
+        // add a single element
+        stack.push(5);
+
+        Expect(stack.isEmpty()).toBe(false);
+    }
+
+    @Test()
+    public stackWithTwoItemsShouldNotBeEmpty(): void {
+        let stack = new Stack();
+
+        // add a single element
+        stack.push(5);
+        stack.push(5);
+
+        Expect(stack.isEmpty()).toBe(false);
+    }
+
+}
