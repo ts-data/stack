86d5d1bad8a62b7c84a6f46ce50cdc44f5ba21fc
James Monger <jameskmonger@hotmail.co.uk>
2016-08-18 00:22:25 +0100

    Implement pop and push

--- stack.ts
+++ stack.ts
@@ -1,5 +1,7 @@
 export class Stack <TData> {
 
+    private _topNode: Node<TData> = undefined;
+
     public count(): number {
         return undefined;
     }
@@ -9,11 +11,30 @@ export class Stack <TData> {
     }
 
     public push(value: TData): void {
-
+        // create a new Node and add it to the top
+        let node = new Node<TData>(value, this._topNode);
+        this._topNode = node;
     }
 
     public pop(): TData {
-        return undefined;
+        // remove the top node from the stack.
+        // the node at the top now is the one before it
+        let poppedNode = this._topNode;
+        this._topNode = poppedNode.previous;
+
+        return poppedNode.data;
+    }
+
+}
+
+class Node<T> {
+
+    previous: Node<T>;
+    data: T;
+
+    constructor (data: T, previous: Node<T>) {
+        this.previous = previous;
+        this.data = data;
     }
 
 }
