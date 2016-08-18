a2156fde785f88b4171f9403a056a00a832d28c4
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-18 08:53:46 +0100

    Add stack.d.ts

---
+++ stack.d.ts
@@ -0,0 +1,7 @@
+export class Stack<TData> {
+    public count(): number;
+    public isEmpty(): boolean;
+    public push(value: TData): void;
+    public pop(): TData;
+    public peek(): TData;
+}
